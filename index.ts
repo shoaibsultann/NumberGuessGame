fdb0f8535b1f7244302fbe15a0f0c7308afa4016
shoaibawan <shoaibawan0002@gmail.com>
2023-01-29 14:46:10 +0500

    Guess number game working

---
+++ index.ts
@@ -0,0 +1,42 @@
+import inquirer from "inquirer";
+
+console.log(`Game: Lets guess a number between 1 to 10 in 3 tries`);
+
+let randNum = Math.floor(Math.random() * 10 + 1);
+// console.log(`random num is: ${randNum}`);
+
+let actualAnswer: number = randNum; //random number auto generated
+let numTries: number = 3;
+let play: Boolean= true;
+
+interface AnswersType {
+  meraGuess: number;
+}
+while(play){
+while (numTries > 0) {
+  const answer: AnswersType = await inquirer.prompt([
+    {
+      name: "meraGuess",
+      message: "Number dalo Bahi",
+      type: "number",
+    },
+  ]);
+
+  // console.log(answer);
+  if (answer.meraGuess == actualAnswer) {
+    console.log("Hurrah! You gussed it right. Game Over");
+    numTries = 0;
+  } else {
+    console.log("You guessed it wrong");
+    if (actualAnswer > answer.meraGuess) {
+      console.log("Think Higher");
+    } else {
+      console.log("Think Lower");
+    }
+    console.log(`You have ${numTries - 1} left`);
+  }
+  numTries--; //numTries = numTries-1
+
+}
+ 
+}
